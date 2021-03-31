b28d9b3e7fd4f8486306612a998f8d01bf3ef080
NGalderisi <nathan@galderisi.com>
2021-03-31 14:48:38 -0400

    Added check to see if if it is sorted correctly. It is currently hard coded and not flexible based on size of input.

--- ArraySorting/ArraySorting/Program.cs
+++ ArraySorting/ArraySorting/Program.cs
@@ -7,30 +7,46 @@ namespace ArraySorting
     {
         static void Main(string[] args)
         {
-            int[] numbers = { 5, 4, 3, 2, 1 };
+            int[] numbers = {5, 4, 3, 2, 1 };
             Console.WriteLine(numbers);
             int [] final = BogoSort(numbers);
             for (int i = 0; i < final.Length; i++)
-                Console.Write($"{final[i]}, ");
+                Console.Write($"{final[i]} ");
 
         }
 
         static int[] BogoSort (int [] numbers)
         {
             Random rnd = new Random();
+            bool unsorted = true;
+            int counter = 1;
             int[] checkOrder = new int[numbers.Length];
-            List<int[]> ordersChecked = new List<int[]>();
-            List<int> arrayPlaceChecker = new List<int>();
 
-            for (int i = 0; i < numbers.Length;)
+            while (unsorted)
             {
-                int arrayPlace = rnd.Next(0, numbers.Length);
-                if (!arrayPlaceChecker.Contains(arrayPlace))
+                checkOrder = new int[numbers.Length];
+                List<int> arrayPlaceChecker = new List<int>();
+
+                for (int i = 0; i < numbers.Length;)
                 {
-                    arrayPlaceChecker.Add(arrayPlace);
-                    checkOrder[i] = numbers[arrayPlace];
-                    i++;
+                    int arrayPlace = rnd.Next(0, numbers.Length);
+                    if (!arrayPlaceChecker.Contains(arrayPlace))
+                    {
+                        arrayPlaceChecker.Add(arrayPlace);
+                        checkOrder[i] = numbers[arrayPlace];
+                        i++;
+                    }
                 }
+                if (checkOrder[0] < checkOrder[1] && checkOrder[1] < checkOrder[2] && checkOrder[2] < checkOrder[3] && checkOrder[3] < checkOrder[4])
+                {
+                    unsorted = false;
+                    Console.WriteLine($"It took {counter} attempt(s) to sort this correctly");
+                }
+                else
+                { 
+                    counter++;
+                }
+
             }
 
             return checkOrder;
