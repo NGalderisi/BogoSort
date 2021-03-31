008b08418b830996687f607425b6e4306d708628
NGalderisi <nathan@galderisi.com>
2021-03-31 14:35:01 -0400

    Added the random generator that scambles the numbers in the array, need to add a way to check if it is sorted correctly

--- ArraySorting/ArraySorting/Program.cs
+++ ArraySorting/ArraySorting/Program.cs
@@ -8,24 +8,32 @@ namespace ArraySorting
         static void Main(string[] args)
         {
             int[] numbers = { 5, 4, 3, 2, 1 };
-            
+            Console.WriteLine(numbers);
+            int [] final = BogoSort(numbers);
+            for (int i = 0; i < final.Length; i++)
+                Console.Write($"{final[i]}, ");
 
         }
 
         static int[] BogoSort (int [] numbers)
         {
             Random rnd = new Random();
+            int[] checkOrder = new int[numbers.Length];
+            List<int[]> ordersChecked = new List<int[]>();
             List<int> arrayPlaceChecker = new List<int>();
 
-            int arrayPlace = rnd.Next(0, numbers.Length);
-            if (arrayPlaceChecker.Contains(arrayPlace))
+            for (int i = 0; i < numbers.Length;)
             {
-                
-                arrayPlaceChecker.Add(arrayPlace);
+                int arrayPlace = rnd.Next(0, numbers.Length);
+                if (!arrayPlaceChecker.Contains(arrayPlace))
+                {
+                    arrayPlaceChecker.Add(arrayPlace);
+                    checkOrder[i] = numbers[arrayPlace];
+                    i++;
+                }
             }
 
-            int[] placeHolder = { 1, 2 };
-            return placeHolder;
+            return checkOrder;
 
         }
     }
