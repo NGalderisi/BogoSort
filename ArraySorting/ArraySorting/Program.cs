8887690e65f23d8faa480d121748ad82e6e4d57a
NGalderisi <nathan@galderisi.com>
2021-03-31 13:34:56 -0400

    Built a quick base

---
+++ ArraySorting/ArraySorting/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ArraySorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 5, 4, 3, 2, 1 };
+            
+
+        }
+
+        static int[] BogoSort (int [] numbers)
+        {
+            Random rnd = new Random();
+
+
+            int[] placeHolder = { 1, 2 };
+            return placeHolder;
+
+        }
+    }
+}
