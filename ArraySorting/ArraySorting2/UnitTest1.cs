967a9fb9747880ce8a1384b1279830ee4db3689e
NGalderisi <nathan@galderisi.com>
2021-04-01 14:30:52 -0400

    Added a few new tests.

--- ArraySorting/ArraySorting2/UnitTest1.cs
+++ ArraySorting/ArraySorting2/UnitTest1.cs
@@ -15,6 +15,21 @@ namespace ArraySortingTests
 ;           int[] sortedtest = Program.BogoSort(testnumbers);
             CollectionAssert.AreEqual(expectedresult, sortedtest);
         }
-        
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int[] testnumbers = { 0, 2, 1 };
+            int[] expectedresult = { 0, 1, 2 };
+            int[] sortedtest = Program.BogoSort(testnumbers);
+            CollectionAssert.AreEqual(expectedresult, sortedtest);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int[] testnumbers = { 1, 0, -1 };
+            int[] expectedresult = { -1, 0, 1 };
+            int[] sortedtest = Program.BogoSort(testnumbers);
+            CollectionAssert.AreEqual(expectedresult, sortedtest);
+        }
     }
 }
